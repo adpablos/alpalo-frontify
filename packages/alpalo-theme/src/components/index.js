0d02118132c2decee441e1d044b0c6b95fc43310
Alejandro de Pablos <adpabloslopez@gmail.com>
2021-04-01 06:48:03 -0400

    Menu included using Link

--- packages/alpalo-theme/src/components/index.js
+++ packages/alpalo-theme/src/components/index.js
@@ -1,13 +1,19 @@
-// File: /packages/alpalo-theme/src/components/index.js
-
 import React from "react"
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
 
 const Root = ({ state }) => {
     return (
         <>
             <h1>Hey there! I am Alex!</h1>
             <p>Current URL from state: {state.router.link}</p>
+            <nav>
+                <Link link="/">Home</Link>
+                <br />
+                <Link link="/page/2">More posts</Link>
+                <br />
+                <Link link="/about-us">About Us</Link>
+            </nav>
         </>
     )
 }
