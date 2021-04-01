25f61174a9e1fe9ee6c599033068db1696acbceb
Alejandro de Pablos <adpabloslopez@gmail.com>
2021-03-31 20:27:46 -0400

    Own theme and more files in git ignore

---
+++ packages/alpalo-theme/src/components/index.js
@@ -0,0 +1,13 @@
+// File: /packages/alpalo-theme/src/components/index.js
+
+import React from "react"
+
+const Root = () => {
+    return (
+        <>
+            <h1>Hey there! I am Alex!</h1>
+        </>
+    )
+}
+
+export default Root
