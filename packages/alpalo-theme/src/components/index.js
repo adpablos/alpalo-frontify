245289fe33bc01ee54895782d678cef1621f8628
Alejandro de Pablos <adpabloslopez@gmail.com>
2021-04-01 21:37:58 -0400

    Styling
    Incorporating some styling specific for some components like Header, Main and HeaderContent

--- packages/alpalo-theme/src/components/index.js
+++ packages/alpalo-theme/src/components/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, Global, css, styled } from "frontity"
 import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 import List from "./list"
 import Post from "./post"
 import Page from "./page"
-import { Global, css } from "frontity"
 
 const Root = ({ state }) => {
     const data = state.source.get(state.router.link)
@@ -14,30 +13,75 @@ const Root = ({ state }) => {
         <>
             <Global
                 styles={css`
-                  html {
-                    font-family: system-ui, Verdana, Arial, sans-serif;
-                  }
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                    }                
+                    html {
+                        font-family: system-ui, Verdana, Arial, sans-serif;
+                    }
                 `}
             />
-            <h1>Hey there! I am Alex!</h1>
-            <p>Current URL from state: {state.router.link}</p>
-            <nav>
-                <Link link="/">Home</Link>
-                <br />
-                <Link link="/page/2">More posts</Link>
-                <br />
-                <Link link="/about-us">About Us</Link>
-            </nav>
-            <hr />
-            <main>
+            <Header>
+                <HeaderContent>
+                    <h1>Hey there! I am Alex!</h1>
+                    <p>Current URL from state: {state.router.link}</p>
+                    <nav>
+                        <Link link="/">Home</Link>
+                        <br />
+                        <Link link="/page/2">More posts</Link>
+                        <br />
+                        <Link link="/about-us">About Us</Link>
+                    </nav>
+                </HeaderContent>
+            </Header>
+            <Main>
                 <Switch>
                     <List when={data.isArchive} />
                     <Post when={data.isPost} />
                     <Page when={data.isPage} />
                 </Switch>
-            </main>
+            </Main>
         </>
     )
 }
 
 export default connect(Root)
+
+const Header = styled.header`
+    background-color: #e5edee;
+    border-width: 0 0 8px 0;
+    border-style: solid;
+    border-color: maroon;
+    
+    h1 {
+        color: #4a4a4a;
+    }
+`
+const HeaderContent = styled.div`
+    max-width: 800px;
+    padding: 2em 1em;
+    margin: auto;
+`
+const Main = styled.main`
+    max-width: 800px;
+    padding: 1em;
+    margin: auto;
+    
+    img {
+        max-width: 100%;
+    }
+    h2 {
+        margin: 0.5em 0;
+    }
+    p {
+        line-height: 1.25em;
+        margin-bottom: 0.75em;
+    }
+    figcaption {
+        color: #828282;
+        font-size: 0.8em;
+        margin-bottom: 1em;
+    }
+`
